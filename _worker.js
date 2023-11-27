ed3ee5e613dbcbae86bb399855c23e0ae9111974
msertv <125521741+msertv@users.noreply.github.com>
2023-11-27 23:30:23 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -4,7 +4,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = '76c2d36e-8d39-11ee-b9d1-0242ac120002';
+let userID = '4a9868a2-c8a0-48fb-b8c7-112651c6df9e';
 
 const proxyIPs = ['cdn-all.xn--b6gac.eu.org', 'cdn.xn--b6gac.eu.org', 'cdn-b100.xn--b6gac.eu.org', 'edgetunnel.anycast.eu.org', 'cdn.anycast.eu.org'];
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
